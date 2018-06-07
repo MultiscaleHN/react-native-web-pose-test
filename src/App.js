ed2914d9993213220708e9e6bd6d51d2aeb82f21
Chris Drackett <chris@drackett.com>
2018-06-07 13:41:27 -0700

    adding initial project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import logo from './logo.svg'
+import './App.css'
+
+import { View, Text } from 'react-native'
+import posed from 'react-native-pose'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <View>
+          <Text>Test Pose!</Text>
+        </View>
+      </div>
+    )
+  }
+}
+
+export default App
